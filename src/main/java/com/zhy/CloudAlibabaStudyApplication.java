684e5fcc5741ed6f48a7b9afd9244d3d746782f4
zhouhongyin <1429855087@qq.com>
2023-03-03 17:36:21 +0800

    init

---
+++ src/main/java/com/zhy/CloudAlibabaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaStudyApplication.class, args);
+    }
+
+}
