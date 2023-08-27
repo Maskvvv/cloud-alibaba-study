939c0bbfbdcefc160328e2a4167e181c0288897c
mask <1429855087@qq.com>
2023-08-27 21:15:50 +0800

    rocketmq 过滤消息 tag 方式

---
+++ rocketmq/src/main/java/com/zhy/rocketmq/filter/TagFilterConsumer.java
@@ -0,0 +1,40 @@
+package com.zhy.rocketmq.filter;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * <p> 过滤消息消费者 </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/8/27 21:14
+ */
+public class TagFilterConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        consumer.subscribe("TagFilterTest", "TagA || TagC");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ rocketmq/src/main/java/com/zhy/rocketmq/filter/TagFilterProducer.java
@@ -0,0 +1,34 @@
+package com.zhy.rocketmq.filter;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * <p> 过滤消息生产者 </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/8/27 21:14
+ */
+public class TagFilterProducer {
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        producer.start();
+
+        String[] tags = new String[] {"TagA", "TagB", "TagC"};
+
+        for (int i = 0; i < 60; i++) {
+            Message msg = new Message("TagFilterTest",
+                tags[i % tags.length],
+                "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+
+        producer.shutdown();
+    }
+}
