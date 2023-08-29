0ddf7ef76cdf314aa92dfad230fdd1f76e9f1594
mask <1429855087@qq.com>
2023-08-29 22:05:16 +0800

    rocketmq spring cloud module init

---
+++ rocket-study/rocketmq-spring-cloud-producer/src/main/java/com/zhy/cloud/producer/RocketmqSpringCloudProducerApplication.java
@@ -0,0 +1,13 @@
+package com.zhy.cloud.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqSpringCloudProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqSpringCloudProducerApplication.class, args);
+    }
+
+}
